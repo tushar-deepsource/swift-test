0a8589a1f0de5b209a51a17c3886496e5a365898
Anto Christopher <anto@deepsource.io>
2023-05-31 15:26:13 +0530

    chore: add test code

---
+++ hello.swift
@@ -0,0 +1,7 @@
+func foo() -> Void {}
+
+// [SW-P1006]: 9
+myString == ""
+
+// [SW-P1007]: 1
+myList.filter { $0 % 2 == 0 }.first
---
+++ world.swift
@@ -0,0 +1,5 @@
+// [SW-P1006]: 9
+myString == ""
+
+// [SW-P1007]: 1
+myList.filter { $0 % 2 == 0 }.first
