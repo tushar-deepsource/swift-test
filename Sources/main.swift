74ef12cc45a1089168a76cb25b1fcda9a8cbf41d
Tushar Sadhwani <86737547+tushar-deepsource@users.noreply.github.com>
2023-10-05 18:29:23 +0530

    Update main.swift

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,11 +1,13 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-books
 
-func addNums(a: Int, b: Int) -> Int {
-  return a + b
+func addNums(a:   Int, b:Int) -> Int {
+     return a + b
 }
 
-func foo() {
+
+
+func  foo() {
   let max = arr.sorted().last // trivia
   let min = /* trivia */ arr.sorted().first
 }
